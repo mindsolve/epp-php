bb52150b6a27f32559c80fde36d77434405013ff
Felix <together+github@felix-ern.de>
2021-01-26 12:07:11 +0100

    Kapitel 4: Übung 4a Farben und Bilder: Lösung eingepflegt

--- 04a_farben_und_bilder/index.php
+++ 04a_farben_und_bilder/index.php
@@ -4,6 +4,38 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <title>PHP-Kurs Kapitel 04</title>
+    <style>
+        <?php if (isset($_POST["back-color"])) { ?>
+        .wrapper {
+            background-color: <?php echo($_POST["back-color"]) ?>;
+        }
+        <?php } ?>
+
+        <?php if (isset($_POST["text-color"])) { ?>
+        p {
+            color: <?php echo($_POST["text-color"]) ?>;
+        }
+        <?php } ?>
+
+        <?php
+
+        switch ($_POST["image"]) {
+            case "murray":
+                $bild = "https://assets.cdn.moviepilot.de/files/58554a4997548bee5cef3437711d8b0368c7375c743bc4a6eeb09578fabe/fill/1200/576/Murmeltier.jpg";
+                break;
+            case "cage":
+                $bild = "https://filmpluskritik.files.wordpress.com/2019/11/nick-cage-conair.jpg";
+                break;
+            case "hanks":
+                $bild = "https://media.tegna-media.com/assets/WQAD/images/0b0bb3e8-97ef-4b1b-8048-6e9249b1cd4f/0b0bb3e8-97ef-4b1b-8048-6e9249b1cd4f_750x422.jpg";
+                break;
+            default:
+                $bild = "https://http.cat/404";
+                break;
+        }
+        ?>
+
+    </style>
 </head>
 <body>
 <div class="wrapper">
@@ -15,18 +47,24 @@
         <form action="index.php" method="post">
             <h4>Welche Hintergrundfarbe soll die Webseite haben?</h4>
             <div class="inputbox">
-                <label><input type="radio" name="back-color" value="bg" checked/>Hellgrün</label>
+                <label><input type="radio" name="back-color" value="lightgreen" checked/>Hellgrün</label>
+                <label><input type="radio" name="back-color" value="orange"/>Orange</label>
+                <label><input type="radio" name="back-color" value="lightblue"/>Hellblau</label>
 
             </div>
 
             <h4>Welche Schriftfarbe ist gewünscht?</h4>
             <div class="inputbox">
-
+                <label><input type="radio" name="text-color" value="black" checked/>Schwarz</label>
+                <label><input type="radio" name="text-color" value="gray"/>Grau</label>
+                <label><input type="radio" name="text-color" value="blueviolet"/>Lila</label>
             </div>
 
             <h4>Welches Foto soll angezeigt werden?</h4>
             <div class="inputbox">
-
+                <label><input type="radio" name="image" value="murray" checked/>Bill Murray</label>
+                <label><input type="radio" name="image" value="cage"/>Nicolas Cage</label>
+                <label><input type="radio" name="image" value="hanks"/>Tom Hanks</label>
             </div>
 
             <div class="inputbox">
@@ -46,7 +84,7 @@
         </p>
 
         <h3>Bild</h3>
-
+        <img src="<?php echo($bild); ?>" alt="Dynamisches Bild"/>
 
     </main>
 </div>

c9c037c04f2645f479b02522e508a0ef7aa3cdbb
Felix <together+github@felix-ern.de>
2021-01-26 11:09:50 +0100

    Kapitel 4: Übung 4a Farben und Bilder: Vorlagen formattiert, CSS korrigiert

--- 04a_farben_und_bilder/index.php
+++ 04a_farben_und_bilder/index.php
@@ -1,55 +1,55 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "utf-8">
-        <link rel="stylesheet" type="text/css"   href="style.css" />
-        <title>PHP-Kurs Kapitel 04</title>
-    </head>
-    <body>
-        <div class = "wrapper">
-            <main>
-                <h1>PHP-Kurs</h1>
-                <h2>Kapitel 04 - Übung 1</h2>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>PHP-Kurs Kapitel 04</title>
+</head>
+<body>
+<div class="wrapper">
+    <main>
+        <h1>PHP-Kurs</h1>
+        <h2>Kapitel 04 - Übung 1</h2>
 
-                <h3>Webseite einrichten</h3>
-                <form action = "index.php" method = "post">
-                    <h4>Welche Hintergrundfarbe soll die Webseite haben?</h4>
-                    <div class = "inputbox">
-                        <label><input type ="radio" name = "back-color" value ="bg" checked/>Hellgrün</label>
-                       
-                    </div>
+        <h3>Webseite einrichten</h3>
+        <form action="index.php" method="post">
+            <h4>Welche Hintergrundfarbe soll die Webseite haben?</h4>
+            <div class="inputbox">
+                <label><input type="radio" name="back-color" value="bg" checked/>Hellgrün</label>
 
-                    <h4>Welche Schriftfarbe ist gewünscht?</h4>
-                    <div class = "inputbox">
-                       
-                    </div>
+            </div>
 
-                    <h4>Welches Foto soll angezeigt werden?</h4>
-                    <div class = "inputbox">
-                      
-                    </div>
+            <h4>Welche Schriftfarbe ist gewünscht?</h4>
+            <div class="inputbox">
 
-                    <div class = "inputbox">
-                        <input type = "submit" name = "submit"/>
-                    </div>
-                </form>
+            </div>
 
-                <h3>Text</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-                    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
-                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore 
-                    magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </p>
+            <h4>Welches Foto soll angezeigt werden?</h4>
+            <div class="inputbox">
 
-                <h3>Bild</h3>
+            </div>
 
-             
-        </main>
-    </div>
+            <div class="inputbox">
+                <input type="submit" name="submit"/>
+            </div>
+        </form>
 
-    </body>
+        <h3>Text</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+            magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+
+        <h3>Bild</h3>
+
+
+    </main>
+</div>
+
+</body>
 </html>

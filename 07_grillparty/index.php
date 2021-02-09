d13bfa985d9399bed7335d2099fa29b303bf72eb
Felix <together+github@felix-ern.de>
2021-02-09 10:12:32 +0100

    Kapitel 7: Grillparty: Aufgaben hinzugefügt

---
+++ 07_grillparty/index.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <link rel="stylesheet" type="text/css"   href="css/style.css" />
+        <title>Grillparty</title>
+    </head>
+    <body>
+        <div class = "wrapper">
+                <main>
+                    <img src = "img/banner_grillparty.jpg">
+                    <h1>Einladung zur Grillparty</h1>
+               
+                    <h2>Wir planen eine große Grillparty - sei dabei!</h2>
+                    <p>
+                        Hallo, wir planen eine große Grillparty.
+                        Es gibt reichlich Essen, Trinken und Musik.
+                        Die Feier soll am Samstag in zwei Wochen ab 15 Uhr bei uns im Garten stattfinden.
+                        Ihr seid herzlich eingeladen. Wir würden uns sehr über Deine/Eure Zusage freuen.
+                        Außerdem fänden wir es toll, wenn jeder eine Kleinigkeit zum Büffet beitragen könnte. 
+                        (Grillgut, Salat, Brot, Dips, Nachtisch, ...).
+                        Grill und Getränke besorgen wir!
+                        Bitte tragt euch in die Liste ein!
+                    </p>
+                    <p><strong>Liebe Grüße, Anna und Thilo.</strong></p>
+                
+                    <h2>Bisherige Zusagen:</h2>
+                    <table>
+                        <thead>
+                            <tr><th>Nummer</th><th>Vorname</th><th>Nachname</th><th>Ich bringe mit</th></tr>
+                        </thead>
+                        <tbody>
+                        
+                          <!-- Ausgabe der Datensätze */  -->
+
+                        </tbody>
+                    </table>                    
+               
+            </main>
+        </div>
+    </body>
+</html>
